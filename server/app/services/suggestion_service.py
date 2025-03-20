8eea5f6c24a0b5b9b67fe2475e1c49447d9af756
HydroLink <nrucinski01@gmail.com>
2025-03-19 21:04:09 -0400

    updated run.py and suggestion service params

--- server/app/services/suggestion_service.py
+++ server/app/services/suggestion_service.py
@@ -9,10 +9,7 @@ def getSuggestion(
     prompt: str,
     vendor: str = vendors.Google,
     model_name: str = "codellama",
-    temperature: float = 0.2,
-    top_p: float = 1,
-    top_k: int = 0,
-    max_tokens: int = 256,
+    model_params: dict = None,
     is_correct: bool = True
 ):
     """
